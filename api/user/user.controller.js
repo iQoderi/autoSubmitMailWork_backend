a2f55e16367e6de560728655d2d83dc244c75808
qoder <qoder@hrsoft.net>
2016-10-11 13:20:23 +0800

    获取用户信息完成

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -7,7 +7,26 @@ const mongoose=require('mongoose');
 const userModel=require('../../models/user.model');
 const User=mongoose.model('User');
 
-
+/**
+ * 获取用户信息
+ */
 exports.getUserInfo=function *() {
-  this.body=this.user;
+  const user={
+    id:this.user.id,
+    username:this.user.username
+  };
+  this.body={
+    code:0,
+    data:{
+      user:user
+    }
+  }
+};
+
+/**
+ * 删除用户
+ */
+exports.rmUser=function *() {
+  const condition={id:this.parmas.id};
+  const user=User.delete(condition);
 };
